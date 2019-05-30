ac96a7158b72d01a9e37d09456d0b9495b9221a8
maxjulianog <maxjulianog@gmail.com>
2019-05-30 17:26:05 -0300

    Inicialização do repositório

---
+++ Model/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Filme
+    {
+        public int Id;
+        public string Nome;
+        public string Categoria;
+        public bool Curtiu;
+        public DateTime Duracao;
+        public decimal avaliacao;
+        public bool TemSequencia;
+
+
+
+    }
+}
